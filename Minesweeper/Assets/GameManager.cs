9a853d74e93260d468a2932e0437644dcc64913a
Jesse Riggins <jesse.riggins98@gmail.com>
2020-12-22 05:14:24 -0500

    Game now generates a matrix of tiles of a specified size.

--- Minesweeper/Assets/GameManager.cs
+++ Minesweeper/Assets/GameManager.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int sizeX = 10;
+    public int sizeY = 10;
+    public int numMines = 5;
+
+
+    public GameObject[][] gameBoard;
+
+    public GameObject tile;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameBoard = new GameObject[sizeX][];
+        for (int i = 0; i < sizeX; i++)
+        {
+            GameObject[] tileColumn = new GameObject[sizeY];
+
+            for (int j = 0; j < sizeY; j++)
+            {
+                GameObject newTile =  Instantiate(tile, new Vector3(i, j, 0), new Quaternion(0,0,0,0), this.gameObject.transform) as GameObject;
+                newTile.name = "Tile (" + i + ", " + j + ")";
+                newTile.GetComponent<Tile>().coordX = i;
+                newTile.GetComponent<Tile>().coordY = j;
+            }
+
+            gameBoard[i] = tileColumn;
+        }
+
+        //gameBoard = new GameObject[sizeX][sizeY];
     }
 
     // Update is called once per frame
--- Minesweeper/Assets/Tile.cs
+++ Minesweeper/Assets/Tile.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Tile : MonoBehaviour
 {
+    public int coordX = 0;
+    public int coordY = 0;
+
+    public bool isMine = false;
+    public bool isFlagged = false;
+    public bool isQuestioned = false;
+    public bool isRevealed = false;
+    public int nearbyMines = 0;
+
+    TextMeshProUGUI text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        string myText = "";
+        if (isRevealed)
+        {
+            myText = nearbyMines.ToString();
+            if (isMine)
+                myText = "M";
+        }
+        else
+        {
+            if (isFlagged)
+                myText = "!";
+            else if (isQuestioned)
+                myText = "?";
+        }
         
+
+
+        text.SetText(myText);
     }
 }
