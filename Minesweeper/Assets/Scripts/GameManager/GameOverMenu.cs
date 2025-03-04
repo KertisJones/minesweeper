1f6401646f5eef6d6e41dea76bfd692ddb88da99
KertisJones <jesse.riggins98@gmail.com>
2025-03-04 16:02:52 -0500

    Adds separate restart and endless buttons to the GameOverMenu script, but comments them out because it didn't actually work right. I guess? I'm committing this months later so I'm not sure.

--- Minesweeper/Assets/Scripts/GameManager/GameOverMenu.cs
+++ Minesweeper/Assets/Scripts/GameManager/GameOverMenu.cs
@@ -13,6 +13,9 @@ public class GameOverMenu : MonoBehaviour
 
     public Material normalMaterial;
     public Material burningMaterial;
+
+    //public GameObject restartButton;
+    //public GameObject endlessButton;
     private void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
@@ -22,11 +25,34 @@ public class GameOverMenu : MonoBehaviour
         gameOverText.text = GameManager.GetTranslation("UIText", "Menu GameOver");
         gameOverTextSubtitle.text = "";
         backgroundBlurImage.material = normalMaterial;
+
+        //restartButton.SetActive(true);
+        //endlessButton.SetActive(false);
     }
     public void SetDeathBurn()
     {
         gameOverText.text = GameManager.GetTranslation("UIText", "Menu GameOver Burn");
         gameOverTextSubtitle.text = GameManager.GetTranslation("UIText", "Menu GameOver BurnSubtitle");
         backgroundBlurImage.material = burningMaterial;
+
+        //restartButton.SetActive(true);
+        //endlessButton.SetActive(false);
     }
+    /*public void SetGamemodeComplete()
+    {
+        // gameModeNameComplete
+        string gameMode = gm.gameMods.gameModeName;
+        if (gm.gameMods.gameModeDisplayName != "")
+            gameMode = gm.gameMods.gameModeDisplayName;
+
+        string localizedText = GameManager.GetTranslation("UIText", "GameMode " + gameMode); // Returns translation of game mode name, ex. "Marathon"
+
+        gameOverText.text = localizedText + " " + GameManager.GetTranslation("UIText", "GameMode Complete") + "!";
+
+        gameOverTextSubtitle.text = "";
+        backgroundBlurImage.material = normalMaterial;
+
+        endlessButton.SetActive(true);
+        restartButton.SetActive(false);
+    }*/
 }
